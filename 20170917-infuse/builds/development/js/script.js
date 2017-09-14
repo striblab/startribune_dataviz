aa6446e3e7344647ddb382d086789a900276fe87
Frey Hargarten <jeff.hargarten@startribune.com>
2017-09-14 11:14:29 -0500

    updates

--- 20170917-infuse/builds/development/js/script.js
+++ 20170917-infuse/builds/development/js/script.js
@@ -1,4 +1,17 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+$.urlParam = function(name){
+  var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+  if (results != null) { return results[1] || 0; }
+  else { return null; }
+}
+
+var selected = $.urlParam('chart');
+
+if (selected != null){
+$(".slide").hide();
+$("#" + selected).show();
+}
+
 // d3.json('./data/timeline.json', function(error, dataLoad) {
 d3.json('http://googlescript.startribune.com/?macro=AKfycbw_cqdXZADky_zHS3pi9aBL2S3-514vlxJkcnv5TJ1z9sxCqPY&id=1_XVZfr5kc9uC38uSqKZH4ZV5t92W2XlknQf0L_7XVtg&sheet=events', function(error, dataLoad) {
 
